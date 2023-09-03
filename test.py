06f68ded8f7193709de8f97aef8a758ac8b4328e
divandia <106718176+Cvandia@users.noreply.github.com>
2023-09-03 16:26:31 +0800

    📃 docs: 修改版本号，和测试文件

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ from dimtown import Search
 import asyncio
 sc = Search("鬼灭之刃")
 loop = asyncio.get_event_loop()
-list_ = loop.run_until_complete(sc.get_search_list())
-img_list = loop.run_until_complete(sc.get_search_img(list_[3]))
+list_ = loop.run_until_complete(sc.get_img_list())
+img_list = loop.run_until_complete(sc.get_img_url(list_[3]))
 print(img_list)
